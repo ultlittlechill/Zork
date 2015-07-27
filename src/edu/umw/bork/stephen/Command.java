b6a20470a018e967bde1d80106ef5fb458c891f3
Stephen Davies <stephen@umw.edu>
2015-07-27 13:36:22 -0400

    (Simplify Command.)

--- src/edu/umw/bork/stephen/Command.java
+++ src/edu/umw/bork/stephen/Command.java
@@ -2,17 +2,6 @@
 package edu.umw.bork.stephen;
 
 public class Command {
-    public static void main(String args[]) {
-        try {
-            new Command().doIt();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void doIt() throws Exception {
-         
-    }
 
     public String execute() {
         return "That was good, thanks.";
