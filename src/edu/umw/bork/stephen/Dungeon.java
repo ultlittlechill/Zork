daa803c03cf854a3652e1364b71db5971db57b31
Stephen Davies <stephen@umw.edu>
2015-07-29 11:08:50 -0400

    Make compatible with v1 requirements.

--- src/edu/umw/bork/stephen/Dungeon.java
+++ src/edu/umw/bork/stephen/Dungeon.java
@@ -1,20 +1,24 @@
 
 package edu.umw.bork.stephen;
 
+import java.util.ArrayList;
 
 public class Dungeon {
 
     private String name;
     private Room entry;
+    private ArrayList<Room> rooms;
 
     Dungeon(String name, Room entry) {
         this.name = name;
         this.entry = entry;
+        rooms = new ArrayList<Room>();
     }
 
     // for persistence, a big ol constructor will load from file.
     
     public Room getEntry() { return entry; }
     public String getName() { return name; }
+    public void add(Room room) { rooms.add(room); }
 
 }
