ec83406b8ab4a41af8c9c177bb634da948dcf13f
Stephen Davies <stephen@umw.edu>
2015-08-04 01:54:29 -0400

    Go with hashtables in v1.

--- src/edu/umw/bork/stephen/Dungeon.java
+++ src/edu/umw/bork/stephen/Dungeon.java
@@ -1,22 +1,22 @@
 
 package edu.umw.bork.stephen;
 
-import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class Dungeon {
 
     private String name;
     private Room entry;
-    private ArrayList<Room> rooms;
+    private Hashtable<String,Room> rooms;
 
     Dungeon(String name, Room entry) {
         this.name = name;
         this.entry = entry;
-        rooms = new ArrayList<Room>();
+        rooms = new Hashtable<String,Room>();
     }
 
     public Room getEntry() { return entry; }
     public String getName() { return name; }
-    public void add(Room room) { rooms.add(room); }
+    public void add(Room room) { rooms.put(room.getTitle(), room); }
 
 }
