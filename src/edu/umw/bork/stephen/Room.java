0bd14d28892a40779c8fd9fe16477adf5681a9fa
Stephen Davies <stephen@umw.edu>
2015-08-22 15:43:18 -0400

    Remove traces of future items.

--- src/edu/umw/bork/stephen/Room.java
+++ src/edu/umw/bork/stephen/Room.java
@@ -8,12 +8,10 @@ public class Room {
     private String title;
     private String desc;
     private boolean beenHere;
-    //private ArrayList<Item> contents;
     private ArrayList<Exit> exits;
 
     Room(String title) {
         this.title = title;
-        //contents = new ArrayList<Item>();
         exits = new ArrayList<Exit>();
         beenHere = false;
     }
@@ -29,9 +27,6 @@ public class Room {
         } else {
             description = title + "\n" + desc + "\n";
         }
-        //for (Item item : contents) {
-        //    description += "There is a " + item.getName() + " here.\n";
-        //}
         for (Exit exit : exits) {
             description += exit.describe() + "\n";
         }
