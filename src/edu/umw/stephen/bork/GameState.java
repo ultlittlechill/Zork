8bfbc44e2ed2d7b7ca651d5ae0cd1a2ff9cfd195
Stephen <stephen@umw.edu>
2015-10-03 14:01:56 -0400

    Change default filename to not be in /tmp.

--- src/edu/umw/stephen/bork/GameState.java
+++ src/edu/umw/stephen/bork/GameState.java
@@ -17,7 +17,7 @@ public class GameState {
         }
     }
 
-    static String DEFAULT_SAVE_FILE = "/tmp/bork_save";
+    static String DEFAULT_SAVE_FILE = "bork_save";
     static String SAVE_FILE_EXTENSION = ".sav";
     static String SAVE_FILE_VERSION = "Bork v3.0 save data";
 
