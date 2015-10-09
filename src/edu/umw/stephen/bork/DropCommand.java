32ed10e0decd235dd1c4f071e26f12c1ed36930d
Stephen <stephen@umw.edu>
2015-10-09 14:21:51 -0400

    Implement "drop all".

--- src/edu/umw/stephen/bork/DropCommand.java
+++ src/edu/umw/stephen/bork/DropCommand.java
@@ -1,6 +1,8 @@
 
 package edu.umw.stephen.bork;
 
+import java.util.ArrayList;
+
 class DropCommand extends Command {
 
     private String itemName;
@@ -13,14 +15,31 @@ class DropCommand extends Command {
         if (itemName == null || itemName.trim().length() == 0) {
             return "Drop what?\n";
         }
+        if (itemName.equals("all")) {
+            ArrayList<String> inventory = (ArrayList<String>) 
+                GameState.instance().getInventoryNames();
+            if (inventory.size() == 0) {
+                return "You're not carrying anything.\n";
+            }
+            String retval = "";
+            for (String name : inventory) {
+                retval += dropItemNamed(name);
+            }
+            return retval;
+        } else {
+            return dropItemNamed(itemName);
+        }
+    }
+
+    private String dropItemNamed(String name) {
         try {
             Item theItem = GameState.instance().getItemFromInventoryNamed(
-                itemName);
+                name);
             GameState.instance().removeFromInventory(theItem);
             GameState.instance().getAdventurersCurrentRoom().add(theItem);
-            return TakeCommand.capitalize(itemName) + " dropped.\n";
+            return TakeCommand.capitalize(name) + " dropped.\n";
         } catch (Item.NoItemException e) {
-            return "You don't have a " + itemName + ".\n";
+            return "You don't have a " + name + ".\n";
         }
     }
 }
