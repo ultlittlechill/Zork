22e854807566bd11f560d0d502ce4f20b45dcf09
Austin Shannon <ashannon@rosemary@umw.edu>
2015-11-04 18:16:28 -0500

    Java doc'd LookCommand

--- src/edu/umw/cpsc240fall2015team8/zork/LookCommand.java
+++ src/edu/umw/cpsc240fall2015team8/zork/LookCommand.java
@@ -1,11 +1,20 @@
 
 package edu.umw.cpsc240fall2015team8.zork;
 
+/**
+Gives the description of the current Room.
+*/
 class LookCommand extends Command {
 
+    /**
+	Creates a new LookCommand.
+    */
     LookCommand() {
     }
 
+    /**
+	Returns a string containing the description of the current Room.
+    */
     public String execute() {
         return GameState.instance().getAdventurersCurrentRoom().
             describe(true) + "\n";
