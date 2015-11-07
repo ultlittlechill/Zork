051528a065167f43db96fec05a36f607b2b4fe4b
Lucas Duty <lduty@rosemary@umw.edu>
2015-11-06 20:02:54 -0500

    4 4 4 4

---
+++ src/edu/umw/cpsc240fall2015team8/zork/ScoreCommand.java
@@ -0,0 +1,20 @@
+package edu.umw.cpsc240fall2015team8.zork;
+
+
+/** Contains the methods that will allow a player to check their numerical score.
+@author Lucas 
+*/
+class ScoreCommand extends Command {
+
+/** initialize a new scorecommand object
+*/
+ScoreCommand() {
+
+}
+/** When the player inputs a score command the player's current score is displayed on the screen along with a corresponding message. 
+*/
+public String execute() {
+
+}
+
+}
