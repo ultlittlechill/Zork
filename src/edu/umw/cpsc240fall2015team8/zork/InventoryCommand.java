9c2fb6ebabd7b4ff47add46d169f77e7d8f5440e
Austin Shannon <ashannon@rosemary@umw.edu>
2015-11-06 19:27:28 -0500

    test fix

--- src/edu/umw/cpsc240fall2015team8/zork/InventoryCommand.java
+++ src/edu/umw/cpsc240fall2015team8/zork/InventoryCommand.java
@@ -2,13 +2,13 @@
 package edu.umw.cpsc240fall2015team8.zork;
 
 import java.util.ArrayList;
-/** Holds the method used to open the players inventory and allow them to look at it's contents */
-/**  @author Lucas */
+/** Holds the method used to open the players inventory and allow them to look at it's contents.
+@author Lucas */
 class InventoryCommand extends Command {
 
     InventoryCommand() {
     }
-/** opens the players inventory, if it is empty then it returns a message that mentions this, otherwise it returns the names of the items in their inventory */
+/** opens the players inventory, if it is empty then it returns a message that mentions this, otherwise it returns the names of the items in their inventory. */
     public String execute() {
         ArrayList<String> names = GameState.instance().getInventoryNames();
         if (names.size() == 0) {
