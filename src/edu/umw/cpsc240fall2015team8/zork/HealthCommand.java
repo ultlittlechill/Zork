e8516c9d31e6a51a89ee116b1564a99ba5accee3
Jeffrey Wallhermfechtel <jwallher@mail.umw.edu>
2015-11-11 21:30:42 -0500

    added healthCommand

--- src/edu/umw/cpsc240fall2015team8/zork/HealthCommand.java
+++ src/edu/umw/cpsc240fall2015team8/zork/HealthCommand.java
@@ -7,15 +7,22 @@ Allows the player to check their fuzzy health.
 */
 class HealthCommand extends Command {
 
-	/**
-		Creates a new HealthCommand.
-	*/
-HealthCommand() {
-}
+    /**Creates a new HealthCommand.*/
+    HealthCommand() {}
 
-/** Returns a string that describes the player's health level. 
-*/
-String execute(){}
-
+    /** Returns a string that describes the player's health level.*/
+    String execute(){
+	health = GameState.instance().getHealth();
+	if(health>=70)
+		return "You stand tall and strong";
+	else if(health>=50)
+		return "You feel weak, but are still putting up a fight";
+	else if(health>=25)
+		return "You are exhausted and bleeding";
+	else if(health>=10)
+		return "You are leaving a blood trail everywhere you walk.";
+	else if(health>=0)
+		return "You have lost a lot of blood. You may be missing a limb or two, but you're too disoriented to know for sure. Youre about to die."; 
+    }
 
 }
